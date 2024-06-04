0a163fe985c4cf71771025f9929df2a0e8bf4760
clstyn <fiorenzacelestyn@gmail.com>
2024-06-05 00:05:52 +0700

    create user model and post model

---
+++ models/post.model.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const subSchemaBahan = new mongoose.Schema({
+  nama: {
+    type: String,
+  },
+  satuan: {
+    type: String,
+  },
+  kalori: {
+    type: Number,
+  },
+});
+
+const subSchemaLangkah = new mongoose.Schema({
+  text: {
+    type: String,
+  },
+});
+
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  picUrl: {
+    type: String,
+    required: true,
+  },
+  portion: {
+    type: Number,
+    required: true,
+  },
+  cookmin: {
+    type: Number,
+    required: true,
+  },
+  bahan: {
+    type: [subSchemaBahan],
+  },
+  langkah: {
+    type: [subSchemaLangkah],
+  },
+  like: {
+    type: Number,
+    default: 0,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const Post = mongoose.model("Post", PostSchema);
+module.exports = Post;
---
+++ models/user.model.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const subSchemaBMI = new mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  value: {
+    type: Number,
+  },
+  category: {
+    type: String,
+  },
+});
+
+const subSchemaAKG = new mongoose.Schema({
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  value: {
+    type: Number,
+  },
+});
+
+const UserSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  point: {
+    type: Number,
+    default: 0,
+  },
+  latestCheckin: {
+    type: Date,
+    default: Date.now,
+  },
+  bmis: {
+    type: [subSchemaBMI],
+    default: [],
+  },
+  akgs: {
+    type: [subSchemaAKG],
+    default: [],
+  },
+  likedRecipes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "Post",
+    default: [],
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  console.log(this.email);
+  if (validator.isEmail(this.email)) {
+    next();
+  } else {
+    next(new Error("Email tidak valid"));
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
