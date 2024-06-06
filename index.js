19ca381b71b9ef839fee0ac7091d0353c29c1b45
clstyn <fiorenzacelestyn@gmail.com>
2024-06-06 19:45:30 +0700

    fix static files

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 });
 
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/auth", require("./routes/auth.route"));
