10a5dbfbe43138f02b7a15c6d199765c6dcb0454
Maura Yufi Septania Putri <79208207+MauraYufi@users.noreply.github.com>
2024-06-08 00:52:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000' }));
-
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: 'GET,POST,PUT,DELETE', // Allow specific HTTP methods
+  allowedHeaders: 'Content-Type,Authorization', // Allow specific headers
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
